8622d593302c6b4fb08005f1d2a18fd8fa10379e
Teerayut Hiruntaraporn <teerayut.h@throughwave.co.th>
2018-05-31 12:50:02 +0700

    Initialize a project with sample tests

---
+++ test/test-string-calculator.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+
+const { expect } = chai;
+
+// Start test cases for Example Behaviors
+function itAlwaysTrue() {
+  // Chai API Reference can be found at http://www.chaijs.com/api/bdd/
+  expect(true).to.be.true;
+}
+
+function itAlwaysBe2() {
+  expect(2).to.be.equal(2);
+}
+
+// Start Example Behaviors
+function exampleBehaviors() {
+  it('should be true', itAlwaysTrue);
+  it('should be equal 2', itAlwaysBe2);
+}
+
+// Start Describe
+describe('Example', exampleBehaviors);
+
